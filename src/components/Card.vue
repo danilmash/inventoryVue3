ab9d480cee00ea34153a1cb467c231b9718e3d47
danilmash <danil.mashentsev@mail.ru>
2024-07-30 00:48:47 +0300

    InventoryPage layout and Card Component

---
+++ src/components/Card.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="card"></div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.card {
+    background-color: var(--card-background);
+    border: 1px solid var(--border-color);
+    border-radius: 12px;
+}
+</style>
--- src/pages/InventoryPage.vue
+++ src/pages/InventoryPage.vue
@@ -1,7 +1,34 @@
 <template>
-  <div class="page"></div>
+    <div class="page inventory-page">
+        <div class="wrapper inventory-page__wrapper">
+            <div class="inventory-page__grid">
+                <Card class="inventory-page__profile"></Card>
+                <div style="background-color: #4d4d4d; aspect-ratio: 1"></div>
+            </div>
+            <Card class="inventory-page__footer-card"></Card>
+        </div>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Card from "@/components/Card.vue";
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.inventory-page {
+    &__wrapper {
+        max-width: 900px;
+    }
+
+    &__grid {
+        height: fit-content;
+        display: grid;
+        gap: 12px;
+        grid-template-columns: 250px minmax(500px, 800px);
+    }
+
+    &__footer-card {
+        margin-top: 24px;
+    }
+}
+</style>
